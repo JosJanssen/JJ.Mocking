4ffeca4dac002daab3bf2e2a527191637a487f54
Jos Janssen <josj@axerrio.com>
2017-03-14 16:54:19 +0100

    Add project files.

---
+++ JJ.Mocking/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJ.Mocking
+{
+    public class Class1
+    {
+    }
+}
